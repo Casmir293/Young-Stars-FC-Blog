e599ecb16e19a2ccdc9a65078bcb58f9f1756350
Casmir Onyeka <casmir293@gmail.com>
2024-05-20 23:44:41 +0100

    feat: restructured files and folders.

--- src/views/posts/index.php
+++ src/views/posts/index.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <?php include_once('../templates/bootstrap_css.php'); ?>
+</head>
+
+<body>
+    <?php include_once('../templates/header.php'); ?>
+    <!-- <?php include_once('../templates/unauthorized_header.php'); ?> -->
+    <?php include_once('../templates/bootstrap_js.php'); ?>
+</body>
+
+</html>
