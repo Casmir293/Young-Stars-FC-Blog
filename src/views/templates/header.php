9bbabc4cf4fa4190cc1cc1686c63072387191106
Casmir Onyeka <casmir293@gmail.com>
2024-06-10 20:54:58 +0100

    feat: updated navbar  avatar.

--- src/views/templates/header.php
+++ src/views/templates/header.php
@@ -2,6 +2,10 @@
 if (!defined('ROOT_PATH')) {
     define('ROOT_PATH', dirname(dirname(dirname(__FILE__))));
 }
+
+require_once './src/controllers/user_controller.php';
+$postController = new PostController($pdo);
+$user_details = $userController->view_user_profile();
 ?>
 
 <nav class="navbar navbar-expand-lg bg-primary fixed-top" data-bs-theme="dark">
@@ -46,7 +50,7 @@ if (!defined('ROOT_PATH')) {
             <form class="d-flex align-items-center" role="search" action="index.php" method="GET">
                 <input class="form-control me-2" type="search" name="search" placeholder="Search" maxlength="15" aria-label="Search" required>
                 <button class="btn btn-outline-light" type="submit">Search</button>
-                <img src="./assets/images/users/default.png" alt="profile-img" class="profile-pic">
+                <img src="<?= htmlspecialchars($user_details['avatar']) ?>" alt="profile-img" class="profile-pic">
                 <a href="?page=logout">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="logout-svg bi bi-box-arrow-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
