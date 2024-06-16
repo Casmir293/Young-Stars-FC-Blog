4bef47b5b0361c990ca3f92110318cf758708bde
Casmir Onyeka <casmir293@gmail.com>
2024-06-16 01:18:22 +0100

    bugfix: resolved view post bug.

--- src/views/templates/header.php
+++ src/views/templates/header.php
@@ -3,8 +3,8 @@ if (!defined('ROOT_PATH')) {
     define('ROOT_PATH', dirname(dirname(dirname(__FILE__))));
 }
 
-require_once './src/controllers/user_controller.php';
-$postController = new PostController($pdo);
+include_once 'db.php';
+$userController = new UserController($pdo);
 $user_details = $userController->view_user_profile();
 
 // Get current page
@@ -27,92 +27,106 @@ function isActive($page, $currentPage, $queryParam = '', $queryValue = '')
 $current_page = getCurrentPage();
 $category_page = isset($_GET['category']) ? $_GET['category'] : '';
 $page_query = isset($_GET['page']) ? $_GET['page'] : '';
-
 ?>
 
-<nav class="navbar navbar-expand-lg bg-primary fixed-top" data-bs-theme="dark">
-    <div class="container">
-        <a class="navbar-brand" href="index.php"> Y<span class="text-warning">OUN</span>G✨<span class="text-warning">S</span>TAR<span class="text-warning">S✨F</span>C</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link <?= isActive('index', $current_page) ?>" aria-current="page" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= isActive('profile', $page_query, 'page', 'profile') ?>" href="?page=profile">Profile</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle <?= $category_page ? 'active' : '' ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Category
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item <?= $category_page === '' ? 'active' : '' ?>" href="index.php">All</a></li>
-                        <li><a class="dropdown-item <?= $category_page === 'updates' ? 'active' : '' ?>" href="?category=updates">Updates</a></li>
-                        <li><a class="dropdown-item <?= $category_page === 'tournament' ? 'active' : '' ?>" href="?category=tournament">Tournament</a></li>
-                        <li><a class="dropdown-item <?= $category_page === 'fun' ? 'active' : '' ?>" href="?category=fun">Fun</a></li>
-                        <li><a class="dropdown-item <?= $category_page === 'others' ? 'active' : '' ?>" href="?category=others">Others</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle <?= isActive('create_post', $page_query, 'page', 'create_post') ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Staff
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item <?= isActive('create_post', $page_query, 'page', 'create_post') ?>" href="?page=create_post">Add Post</a></li>
-                        <li><a class="dropdown-item" href="#">Role Management</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item logout">
-                    <button class="btn btn-outline-light my-2" type="submit">Logout</button>
-                </li>
-            </ul>
-            <form class="d-flex align-items-center" role="search" action="index.php" method="GET">
-                <input class="form-control me-2" type="search" name="search" placeholder="Search" maxlength="15" aria-label="Search" required>
-                <button class="btn btn-outline-light" type="submit">Search</button>
-                <a href="?page=profile"><img src="<?= htmlspecialchars($user_details['avatar']) ?>" alt="profile-img" class="profile-pic"></a>
-                <a href="?page=logout">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="logout-svg bi bi-box-arrow-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
-                        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
-                    </svg>
-                </a>
-            </form>
-        </div>
-    </div>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
 
-<style>
-    .profile-pic {
-        height: 38px;
-        width: 38px;
-        object-fit: cover;
-        border-radius: 50%;
-        margin-left: 5px;
-        cursor: pointer;
-        display: none !important;
-    }
-
-    .logout-svg {
-        display: none;
-    }
-
-    @media (min-width: 1024px) {
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
         .profile-pic {
-            margin-left: 20px;
-            margin-right: 20px;
-            display: block !important;
+            height: 38px;
+            width: 38px;
+            object-fit: cover;
+            border-radius: 50%;
+            margin-left: 5px;
+            cursor: pointer;
+            display: none !important;
         }
 
         .logout-svg {
-            display: block;
-            cursor: pointer;
-        }
-
-        .logout {
             display: none;
         }
-    }
-</style>
+
+        @media (min-width: 1024px) {
+            .profile-pic {
+                margin-left: 20px;
+                margin-right: 20px;
+                display: block !important;
+            }
+
+            .logout-svg {
+                display: block;
+                cursor: pointer;
+            }
+
+            .logout {
+                display: none;
+            }
+        }
+    </style>
+</head>
+
+<body>
+
+
+
+    <nav class="navbar navbar-expand-lg bg-primary fixed-top" data-bs-theme="dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php"> Y<span class="text-warning">OUN</span>G✨<span class="text-warning">S</span>TAR<span class="text-warning">S✨F</span>C</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link <?= isActive('index', $current_page) ?>" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?= isActive('profile', $page_query, 'page', 'profile') ?>" href="?page=profile">Profile</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle <?= $category_page ? 'active' : '' ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Category
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item <?= $category_page === '' ? 'active' : '' ?>" href="index.php">All</a></li>
+                            <li><a class="dropdown-item <?= $category_page === 'updates' ? 'active' : '' ?>" href="?category=updates">Updates</a></li>
+                            <li><a class="dropdown-item <?= $category_page === 'tournament' ? 'active' : '' ?>" href="?category=tournament">Tournament</a></li>
+                            <li><a class="dropdown-item <?= $category_page === 'fun' ? 'active' : '' ?>" href="?category=fun">Fun</a></li>
+                            <li><a class="dropdown-item <?= $category_page === 'others' ? 'active' : '' ?>" href="?category=others">Others</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle <?= isActive('create_post', $page_query, 'page', 'create_post') ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Staff
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item <?= isActive('create_post', $page_query, 'page', 'create_post') ?>" href="?page=create_post">Add Post</a></li>
+                            <li><a class="dropdown-item" href="#">Role Management</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item logout">
+                        <button class="btn btn-outline-light my-2" type="submit">Logout</button>
+                    </li>
+                </ul>
+                <form class="d-flex align-items-center" role="search" action="index.php" method="GET">
+                    <input class="form-control me-2" type="search" name="search" placeholder="Search" maxlength="15" aria-label="Search" required>
+                    <button class="btn btn-outline-light" type="submit">Search</button>
+                    <a href="?page=profile"><img src="<?= htmlspecialchars($user_details['avatar']) ?>" alt="profile-img" class="profile-pic"></a>
+                    <a href="?page=logout">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="logout-svg bi bi-box-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
+                            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+                        </svg>
+                    </a>
+                </form>
+            </div>
+        </div>
+    </nav>
+</body>
+
+</html>
