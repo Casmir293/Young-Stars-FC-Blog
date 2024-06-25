5e26643266ca0fedf720bb225de5e0eb7add7716
Casmir Onyeka <casmir293@gmail.com>
2024-06-25 19:48:43 +0100

    updated header.

--- src/views/templates/unauthorized_header.php
+++ src/views/templates/unauthorized_header.php
@@ -25,7 +25,7 @@ $category_page = isset($_GET['category']) ? $_GET['category'] : '';
 $page_query = isset($_GET['page']) ? $_GET['page'] : '';
 ?>
 
-<nav class="navbar navbar-expand-lg bg-warning" data-bs-theme="dark">
+<nav class="navbar navbar-expand-lg bg-warning fixed-top" data-bs-theme="dark">
     <div class="container">
         <a class="navbar-brand" href="index.php"> YOUNG✨STARS✨FC</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
